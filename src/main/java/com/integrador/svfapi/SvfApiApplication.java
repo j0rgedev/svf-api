72a765ecd642a85be9584a89d34abc3e14f4d6e0
Jorge Antezana <102924003+jorgevfx@users.noreply.github.com>
2023-04-11 16:22:31 -0500

    First commit

---
+++ src/main/java/com/integrador/svfapi/SvfApiApplication.java
@@ -0,0 +1,13 @@
+package com.integrador.svfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvfApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvfApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/integrador/svfapi/SvfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integrador.svfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvfApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
