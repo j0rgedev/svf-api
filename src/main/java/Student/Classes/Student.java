6fe568fe2188266b51b4643ea980b18bd3796996
wolf3 <wolf3976@gmail.com>
2023-04-13 12:24:44 -0500

    MySQLConnection DATABASE fixed.

--- src/main/java/Student/Classes/Student.java
+++ src/main/java/Student/Classes/Student.java
@@ -1,12 +1,4 @@
 package Student.Classes;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class Student {
-    private String id;
-    private String name;
-    private String lastName;
 }
